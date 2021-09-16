fa1191887e3b408b11b757ccf0d67f6579f87e0c
unknown <sebastv95@gmail.com>
2021-09-15 21:34:23 -0500

    mejoras logos, emociones

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,12 +5,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-    <a href="https://www.sena.edu.co/es-co/Empresarios/Paginas/SENA-INNOVA-2021.aspx" target="_blank"><img style="width: 40%; border-radius: 10px;" src="../../../assets/images/logoInnova.jpg"></a>
+    <a href="https://www.sena.edu.co/es-co/Empresarios/Paginas/SENA-INNOVA-2021.aspx"
+    target="_blank"><img style="width: 40%; border-radius: 10px;" 
+    src="../../../assets/images/logoInnova.jpg"></a>
     </span>
     <div class="socials">
     
       <a href="https://www.facebook.com/priceshoes.co" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="https://twitter.com/PriceShoesCO?fbclid=IwAR1odjnIlEHq0-7IpazaNXEhZaWjhPVkMOHBUwoi6Ng_qUfJGp3_ynNZ17Y" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://twitter.com/PriceShoesCO?fbclid=IwAR1odjnIlEHq0-7IpazaNXEhZaWjhPVkMOHBUwoi6Ng_qUfJGp3_ynNZ17Y"
+       target="_blank" class="ion ion-social-twitter"></a>
     </div>
   `,
 })
