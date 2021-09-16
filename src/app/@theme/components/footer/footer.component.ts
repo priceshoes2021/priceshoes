60bc91722e7d387cec9d70be550e9b9e46a1e707
unknown <sebastv95@gmail.com>
2021-09-15 22:08:24 -0500

    img innova

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">
     <a href="https://www.sena.edu.co/es-co/Empresarios/Paginas/SENA-INNOVA-2021.aspx"
-    target="_blank"><img style="width: 40%; border-radius: 10px;" 
-    src="../../../assets/images/logoInnova.jpg"></a>
+    target="_blank"><img style="width: 30%; border-radius: 10px;" 
+    src="https://lh3.googleusercontent.com/proxy/J9c-2aa4nPm1FdHiLL6tZNCPxbEKKqVES6yPpjtyrlfygj9YjNyVZU_6ZKAY_OihowqdmDW3jH8di9iJ3OHtL2o_4R_pBqZS3smU5lbhdQyQgWJCCYzhA8TOrIH8wMwLMMAhPcfTUxyf040P542O-nka63U"></a>
     </span>
     <div class="socials">
     
