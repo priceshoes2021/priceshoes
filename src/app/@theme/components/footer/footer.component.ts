eb4106bca1512883e99a60b552cf44a6ed78c256
CarlosAlbertoR <carlosalbertorios3@gmail.com>
2021-09-22 12:24:03 -0500

    Correccion en el footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
+  selector: "ngx-footer",
+  styleUrls: ["./footer.component.scss"],
   template: `
-    <span class="created-by">
+    <span class="created-by"> </span>
+    <div class="footer">
+      <div class="support d-flex">
+        <h5>Con el apoyo de:</h5>
+        <img
+          width="160px"
+          src="assets/images/sena-support.jpeg"
+          alt="support"
+        />
+      </div>
+      <div class="socials">
+        <a
+          href="https://www.instagram.com/priceshoes_colombia/"
+          target="_blank"
+          class="ion ion-social-instagram"
+        ></a>
 
-    </span>
-    <div class="socials">
+        <a
+          href="https://www.facebook.com/priceshoes.co"
+          target="_blank"
+          class="ion ion-social-facebook"
+        ></a>
 
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+        <a
+          href="https://twitter.com/PriceShoes"
+          target="_blank"
+          class="ion ion-social-twitter"
+        ></a>
+      </div>
     </div>
   `,
 })
-export class FooterComponent {
-}
+export class FooterComponent {}
